795c4976ac30cbb4bbafabffcd5a09ca44b03506
Dan Lupu <66563952+LDan22@users.noreply.github.com>
2021-02-02 23:21:41 +0200

    knox authentication

--- config/urls.py
+++ config/urls.py
@@ -37,6 +37,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
+
     path('api/swagger/', schema_view.with_ui(
         'swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui(
--- users/views.py
+++ users/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from django.contrib.auth import authenticate, login
+from rest_framework.response import Response
+from rest_framework import permissions
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
+from knox.models import AuthToken
 
-# Create your views here.
+
+class UserAPIView(generics.RetrieveAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated,
+    ]
+
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class RegisterAPIView(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        instance, token = AuthToken.objects.create(user)
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "token": token,
+            "expiry": instance.expiry
+        })
+
+
+class LoginAPIView(KnoxLoginView):
+    serializer_class = LoginSerializer
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, *args, **kwargs):
+        # serializer = self.get_serializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        # user = serializer.validated_data
+        # return Response({
+        #     "user": UserSerializer(user, context=self.get_serializer_context()).data,
+        #     "token": AuthToken.objects.create(user)[1]
+        # })
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPIView, self).post(request, format=None)
+
+    def get_user_serializer_class(self):
+        return UserSerializer
