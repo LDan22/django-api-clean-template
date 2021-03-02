226828a744d51e62b5903ec20c1f5d55928f8c5d
Dan Lupu <66563952+LDan22@users.noreply.github.com>
2021-03-02 14:51:07 +0200

    fix

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
 from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
 from rest_framework import permissions
@@ -39,13 +39,6 @@ class LoginAPIView(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request, *args, **kwargs):
-        # serializer = self.get_serializer(data=request.data)
-        # serializer.is_valid(raise_exception=True)
-        # user = serializer.validated_data
-        # return Response({
-        #     "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        #     "token": AuthToken.objects.create(user)[1]
-        # })
         serializer = AuthTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
